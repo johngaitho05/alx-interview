ad57dd05b968acbafb3b15120a75852a6fbe5b5e
John Gaitho <johngaitho05@gmail.com>
2024-03-20 22:25:34 +0300

    fix requirements issues

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 """
-In a text file, there is a single character H. Your text editor can execute only two operations in this file:
-Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed
+In a text file, there is a single character H. Your text editor can execute
+only two operations in this file:
+Copy All and Paste. Given a number n, write a method that calculates the
+fewest number of operations needed
 to result in exactly n H characters in the file.
 
 Prototype: def minOperations(n)
@@ -10,7 +13,8 @@ Example:
 
 n = 9
 
-H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH => Paste => HHHHHHHHH
+H => Copy All => Paste => HH => Paste =>HHH => Copy All => Paste => HHHHHH =>
+Paste => HHHHHHHHH
 
 Number of operations: 6
 """
@@ -21,18 +25,20 @@ def minOperations(n: int) -> int:
     if n == 1:
         return 0
 
-    # Initialize an array to store the minimum number of operations for each position
-    dp = [0] * (n + 1)
+    # Initialize an array to store the minimum number of operations for each
+    # position
+    dp: list = [0] * (n + 1)
 
     for i in range(2, n + 1):
-        # Initialize the minimum number of operations for the current position as the maximum possible value
+        # Initialize the minimum number of operations for the current position
+        # as the maximum possible value
         dp[i] = float('inf')
 
         # Iterate through all factors of i to find the optimal solution
         for j in range(1, i):
             if i % j == 0:
-                # If j is a factor of i, it means we can copy and paste j times to reach i
+                # If j is a factor of i, it means we can copy and paste j times
+                # to reach i
                 dp[i] = min(dp[i], dp[j] + i // j)
 
     return dp[n] if dp[n] != float('inf') else 0
-
